9a52c329c8e0d152bbbe6fcfd1211f5e287dccf0
Krystian Kalecki <krystian.kalecki@o2.pl>
2021-04-02 15:50:10 +0200

    added service logic

--- src/main/java/pl/kalecki/BankApi/service/AccountService.java
+++ src/main/java/pl/kalecki/BankApi/service/AccountService.java
@@ -2,8 +2,11 @@ package pl.kalecki.BankApi.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.kalecki.BankApi.controller.dto.AccountRequest;
+import pl.kalecki.BankApi.controller.dto.AccountResponse;
 import pl.kalecki.BankApi.repository.AccountRepository;
 import pl.kalecki.BankApi.repository.entity.Account;
+import pl.kalecki.BankApi.service.mapper.AccountMapper;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,23 +16,31 @@ import java.util.Optional;
 public class AccountService {
 
     private final AccountRepository repository;
+    private final AccountMapper mapper;
 
-    public List<Account> findAll() {
-        return repository.findAll();
+    public List<AccountResponse> findAll() {
+        List<Account> all = repository.findAll();
+        return mapper.mapAccountsToAccountResponses(all);
     }
 
 
-    public Account save(Account account) {
-        return repository.save(account);
+    public AccountResponse save(AccountRequest accountRequest) {
+
+        Account account = mapper.mapAccountRequestToAccount(accountRequest);
+        Account savedAccount = repository.save(account);
+
+        return mapper.mapAccountToAccountResponse(savedAccount);
     }
 
 
-    public Optional<Account> findById(Long aLong) {
-        return repository.findById(aLong);
+    public AccountResponse findById(Long id) {
+
+        Account account = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Account with this id does not exists"));
+        return mapper.mapAccountToAccountResponse(account);
     }
 
 
-    public void deleteById(Long aLong) {
-        repository.deleteById(aLong);
+    public void deleteById(Long id) {
+        repository.deleteById(id);
     }
 }
